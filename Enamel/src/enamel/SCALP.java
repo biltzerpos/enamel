85d3af4df9d811608f5e53951a516af35db6fd73
sunnylee0123 <sunnylee0123@gmail.com>
2017-07-20 22:33:05 +0000

    Add files via upload

--- Enamel/src/enamel/SCALP.java
+++ Enamel/src/enamel/SCALP.java
@@ -19,18 +19,17 @@ import com.sun.speech.freetts.VoiceManager;
 
 public class SCALP {
 	
-    static private VoiceManager vm = VoiceManager.getInstance();
-	static private Voice voice = vm.getVoice ("kevin16");
+    static VoiceManager vm = VoiceManager.getInstance();
+	static Voice voice = vm.getVoice ("kevin16");
 	
-	//Right now, the directory to scan is hard-coded. We may eventually fix this with the current program directory.
-	//Also, files AND directories are listed right now. Future implementation will list only files (using FilenameFilter)
-	static File folder = new File("C:/Users/sunny/Documents/github/enamel/Enamel/SampleScenarios");	
-	static List<String> listOfFiles = new LinkedList<String>(Arrays.asList(folder.list()));
-	static List<File> listOfFilePaths = new LinkedList<File>(Arrays.asList(folder.listFiles()));
-	static JFrame SFrame;
-	static String current = "";
+	static File factoryDirectory;
+	static List<String> factoryDirectoryFiles;
+	static File USBBufferDirectory;
+	static List<String> USBBufferDirectoryFiles;
+
+	static JFrame SFrame;	
 	static long debounce = 0;
-	static ArrayList<HWButton> buttonList;
+	static ArrayList<HWButton> buttonList;	
 	
 	public static void main(String[] args) {
 		
@@ -38,16 +37,132 @@ public class SCALP {
 		SFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		SFrame.setVisible(true);
 		
+		String mode = args[0];
+		
 		voice.allocate();
-		try { 
-			Gpio.wiringPiSetup();
-			HWButtonSelector();
+				
+		if (mode == "START_USB_WITH_FILE_1") {
+			String startFilePath = args[1];
+			startPlayer(startFilePath);
+		}
+		
+		else if (mode == "START_FACTORY") {
+			factoryDirectory = new File(args[1]);
+			factoryDirectoryFiles = new LinkedList<String>(Arrays.asList(factoryDirectory.list()));
 			
-		} catch (UnsatisfiedLinkError e) {
-			System.out.println("The system does not support the hardware Braille cell. The program will ignore any code pertaining to the hardware Braille Cell.");
-			keySelector();
+			try { 
+				Gpio.wiringPiSetup();
+				HWButtonSelector();
+				
+			} catch (UnsatisfiedLinkError e) {
+				System.out.println("The system does not support the hardware Braille cell. The program will ignore any code pertaining to the hardware Braille Cell.");
+				keySelector(factoryDirectory, factoryDirectoryFiles);
+			}	
+		}
+		
+		else if (mode == "START_HIGH_LEVEL_SELECTOR") {
+			factoryDirectory = new File(args[1]);
+			factoryDirectoryFiles = new LinkedList<String>(Arrays.asList(factoryDirectory.list()));
+			USBBufferDirectory = new File(args[2]);
+			USBBufferDirectoryFiles = new LinkedList<String>(Arrays.asList(USBBufferDirectory.list()));
+			
+			try { 
+				Gpio.wiringPiSetup();
+				HWButtonSelector();
+				
+			} catch (UnsatisfiedLinkError e) {
+				System.out.println("The system does not support the hardware Braille cell. The program will ignore any code pertaining to the hardware Braille Cell.");
+				highLevelSelector();
+			}
+		}
+		
+		else {
+			System.out.println("Error occured within SCALP.");
 		}
 	}
+	
+	private static void highLevelSelector() {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				SFrame.addKeyListener(new KeyListener() {
+					@Override
+					public void keyPressed(KeyEvent e) {
+						switch (e.getKeyCode()){
+				        case KeyEvent.VK_RIGHT :
+				        	voice.speak("USB buffer directory selected");
+				            System.out.println("USB buffer directory selected");
+				            SFrame.removeKeyListener(this);
+				            keySelector(USBBufferDirectory, USBBufferDirectoryFiles);
+				            break;
+				        case KeyEvent.VK_LEFT :
+				        	voice.speak("Factory directory selected");
+				            System.out.println("Factory directory selected");
+				            SFrame.removeKeyListener(this);
+				            keySelector(factoryDirectory, factoryDirectoryFiles);
+				        	break;
+				        case KeyEvent.VK_SPACE :
+				        	System.out.println("exit without selecting");
+				        	System.exit(0);
+				        	break;
+						}
+					}
+					@Override
+					public void keyReleased(KeyEvent e) {}
+
+					@Override
+					public void keyTyped(KeyEvent e) {}
+				});
+			}
+		});
+	}
+	
+	private static void keySelector(File directory, List<String> directoryFiles) {
+		//give some instructions.
+		//Say name of file - "press spacebar to run this script"
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				SFrame.addKeyListener(new KeyListener() {
+					@Override
+					public void keyPressed(KeyEvent e) {
+						ListIterator<String> li = directoryFiles.listIterator();
+						
+						String currentFile;
+						switch (e.getKeyCode()){
+				        case KeyEvent.VK_LEFT :
+				        	currentFile = li.previous();
+				            voice.speak(currentFile);
+				        	System.out.println(currentFile);
+				            break;
+				        case KeyEvent.VK_RIGHT :
+				        	currentFile = li.next();
+				            voice.speak(currentFile);
+				            System.out.println(currentFile);
+				        	break;
+				        case KeyEvent.VK_ENTER :
+				        	System.out.println("exit without selecting");
+				        	System.exit(0);
+				        	break;
+				        case KeyEvent.VK_SPACE :
+				        	currentFile = li.previous();
+				        	System.out.println("file " + currentFile + " selected");
+				            SFrame.removeKeyListener(this);
+				        	startPlayer(directory.getAbsolutePath() + File.separator + currentFile);
+				        	break;
+						}
+					}
+					@Override
+					public void keyReleased(KeyEvent e) {}
+
+					@Override
+					public void keyTyped(KeyEvent e) {}
+				});
+			}
+		});
+	}	
+	
+	
 	
 	private static void HWButtonSelector() {
 		//give some instructions.
@@ -58,15 +173,14 @@ public class SCALP {
 		buttonList.add(new HWButton(6));
 		buttonList.add(new HWButton(7));
 		
-		ListIterator<String> li = listOfFiles.listIterator();
-			
+		ListIterator<String> li = factoryDirectoryFiles.listIterator();
+				
 		Gpio.wiringPiISR(buttonList.get(0).getGPIOPinNumber(), Gpio.INT_EDGE_FALLING, new GpioInterruptCallback() {
 			@Override
 			public void callback(int pin) {
 					if (System.currentTimeMillis() > debounce) {
 						debounce = System.currentTimeMillis() + 1000L;
-						current = li.previous();
-			            voice.speak(current);
+			            voice.speak(li.previous());
 					}
 				}	
 		});
@@ -75,8 +189,7 @@ public class SCALP {
 			public void callback(int pin) {
 					if (System.currentTimeMillis() > debounce) {
 						debounce = System.currentTimeMillis() + 1000L;
-						current = li.next();
-			            voice.speak(current);
+			            voice.speak(li.next());
 					}
 				}	
 		});
@@ -94,11 +207,12 @@ public class SCALP {
 			@Override
 			public void callback(int pin) {
 					if (System.currentTimeMillis() > debounce) {
+						String curFile = li.previous();
 						debounce = System.currentTimeMillis() + 1000L;
-						current = li.previous();
-			        	System.out.println("file " + current + " selected");
+						
+			        	System.out.println("file " + curFile + " selected");
 			        	removeHWButtons();
-			        	startPlayer();
+			        	startPlayer(curFile);
 					}
 				}	
 		});
@@ -110,55 +224,14 @@ public class SCALP {
 		}
 	}
 	
-	private static void keySelector() {
-		//give some instructions.
-		//Say name of file - "press spacebar to run this script"
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				SFrame.addKeyListener(new KeyListener() {
-					ListIterator<String> li = listOfFiles.listIterator();	
-					@Override
-					public void keyPressed(KeyEvent e) {
-						switch (e.getKeyCode()){
-				        case KeyEvent.VK_RIGHT :
-				            current = li.next();
-				            voice.speak(current);
-				            System.out.println(current);
-				            break;
-				        case KeyEvent.VK_LEFT :
-				        	current = li.previous();
-				            voice.speak(current);
-				            
-				        	System.out.println(current);
-				        	break;
-				        case KeyEvent.VK_ENTER :
-				        	System.out.println("exit without selecting");
-				        	System.exit(0);
-				        	break;
-				        case KeyEvent.VK_SPACE :
-				        	current = li.previous();
-				        	System.out.println("file " + current + " selected");
-				        	startPlayer();
-				        	break;
-						}
-					}
-					@Override
-					public void keyReleased(KeyEvent e) {}
-
-					@Override
-					public void keyTyped(KeyEvent e) {}
-				});
-			}
-		});
-	}	
-	
-	static void startPlayer() {
+	static void startPlayer(String path) {
 		SFrame.setVisible(false);
     	SFrame.dispose();
     	new Thread(new Runnable() {
     		public void run() {
     			ScenarioParser s = new ScenarioParser();
-    			s.setScenarioFile("SampleScenarios/" + current);
+    			voice.deallocate();
+    			s.setScenarioFile(path);
     		}
     	}).start();
 	}
