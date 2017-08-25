c924c3c4f04ce1d8c00d3124dafcc1af1ed5f9fc
sunnylee0123 <sunnylee0123@gmail.com>
2017-08-25 15:25:59 -0400

    Minor changes
    removed weird <<<<<< HEAD stuff that was created when it was pushed.

--- Enamel/src/enamel/ScenarioParser.java
+++ Enamel/src/enamel/ScenarioParser.java
@@ -671,11 +671,7 @@ public class ScenarioParser
         {
             cellNum = Integer.parseInt(fileScanner.nextLine().split("\\s")[1]);
             buttonNum = Integer.parseInt(fileScanner.nextLine().split("\\s")[1]);
-<<<<<<< HEAD
 			sim = new VisualPlayer (cellNum, buttonNum);				           
-=======
-			sim = new TactilePlayer (cellNum, buttonNum);				           
->>>>>>> 16836ed726246cbd194d3c0c850e5be2048c123d
         }
         catch (Exception e)
         {
