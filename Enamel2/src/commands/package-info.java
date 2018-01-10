356dd2ceeca69cbc8e458f46ab6e951e9030ab63
Vassilios Tzerpos <bil@cse.yorku.ca>
2018-01-10 09:30:38 -0500

    Removed Enamel2

--- Enamel2/src/commands/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains all the abstract and concrete commands used by the GUI. There exists
- * exactly one command for each of the command sections in the official Player
- * documentation.
- */
-package commands;
--- Enamel2/testfile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#Find and change the "mode=" text.
-sleep 1
-changemode=$1
-sed -i 's/^mode=.*$/mode=`changemode`/' /home/pi/SCALP.sh
