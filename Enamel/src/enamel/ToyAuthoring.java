481c913444b3f172d4ee2d3c417fe41f89c821a3
Huy Hoang Minh Cu <kingofdragon1998@gmail.com>
2018-01-15 06:40:54 -0500

    hi testing

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -5,5 +5,5 @@ public class ToyAuthoring {
     public static void main(String[] args) { 	  	
     	    ScenarioParser s = new ScenarioParser(true);
     	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
-    }
+    }//hi testing
 }
