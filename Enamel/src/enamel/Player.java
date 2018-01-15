4d8a5bec9dea83f83a2ce97a6e93abd658a5ab60
DamanB <damanb19@my.yorku.ca>
2018-01-15 12:28:46 -0500

    i added something

--- Enamel/src/enamel/Player.java
+++ Enamel/src/enamel/Player.java
@@ -9,7 +9,7 @@ import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 /**
- * Group Members: Sanjay Paraboo, Damanveer Bharaj, Pengyuan Guo.
+ * Group Members: Sanjay Paraboo, Damanveer Bharaj, Pengyuan Guo. Our group has 3 members
  * 
 * This class provides abstract and concrete methods for simulating a braille cell.
 * <p>
