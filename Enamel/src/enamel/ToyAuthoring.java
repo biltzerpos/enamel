ce5af99f012bdf1a9db7fe217d380b89b7cf063a
DSantaguida <dsantaguida@outlook.com>
2018-01-17 14:18:14 -0500

    Added JFileChooser

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -1,9 +1,26 @@
 package enamel;
 
+import java.io.*;
+import java.io.FileFilter;
+
+import javax.swing.*;
+import javax.swing.filechooser.*;
+
 public class ToyAuthoring {
-//hello
-    public static void main(String[] args) { 	  	
-    	    ScenarioParser s = new ScenarioParser(true);
-    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
-    }
+	// hello
+	public static void main(String[] args) {
+		String file = "";
+
+		ScenarioParser s = new ScenarioParser(true);
+		JFileChooser chooser = new JFileChooser(new File("FactoryScenarios\\"));
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt");
+		chooser.setFileFilter(filter);
+		int returnVal = chooser.showOpenDialog(null);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			file = "FactoryScenarios/" + chooser.getSelectedFile().getName();
+
+		}
+
+		s.setScenarioFile(file);
+	}
 }
