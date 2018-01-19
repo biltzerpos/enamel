5964b14e7079ab214e1ac37477f8e1c1d4ebc180
guopy96 <guopy96@my.yorku.ca>
2018-01-19 11:00:33 -0500

    old version

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -2,6 +2,8 @@ package enamel;
 
 public class ToyAuthoring {
 
+	//this this the origin version
+	
     public static void main(String[] args) { 	  	
     	    ScenarioParser s = new ScenarioParser(true);
     	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
