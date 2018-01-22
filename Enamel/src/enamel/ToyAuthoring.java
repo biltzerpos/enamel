23198727c50a9a13433e98ae296f21897d1c8581
ynfle <yoni@fihrer.com>
2018-01-22 13:19:19 -0500

    amessage

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -7,8 +7,7 @@ import javax.swing.*;
 import javax.swing.filechooser.*;
 
 public class ToyAuthoring {
-	// test
-	// hello
+	//changing lines for the master to copy
 	public static void main(String[] args) {
 		String file = "";
 
