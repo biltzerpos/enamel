8bbf6f77f32b1f73cfbee5a0b2427647c5271e1d
cuminh98 <cuminh98@ea54.eecs.yorku.ca>
2018-01-22 12:46:23 -0500

    user can choose scenrio files

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -1,9 +1,51 @@
 package enamel;
 
-public class ToyAuthoring {
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-    public static void main(String[] args) { 	  	
-    	    ScenarioParser s = new ScenarioParser(true);
-    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
+import org.apache.commons.io.FilenameUtils;
+
+import authoringApp.FileParser;
+
+import java.awt.Component;
+import java.io.*;
+import java.util.*;
+
+public class ToyAuthoring {
+	private static JFileChooser fileChooser = new JFileChooser();
+    public static void main(String[] args) {
+    		
+    		Component parent = null;    	
+			File f = new File("FactoryScenarios/");
+
+    		fileChooser.setCurrentDirectory(f);
+			int returnVal = fileChooser.showOpenDialog(parent);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				   String ext= FilenameUtils.getExtension(fileChooser.getSelectedFile().getName());
+	               //System.out.println(ext);
+	               if (!ext.equals("txt") )
+	            	   {
+	            	   final JPanel panel = new JPanel();
+	            	   	
+	            	    JOptionPane.showMessageDialog(panel, "Could not open file, Wrong file type", "Error", JOptionPane.ERROR_MESSAGE);
+
+	            	   }
+	               else
+	               {
+	            	   final JPanel panel = new JPanel();
+
+	            	    JOptionPane.showMessageDialog(panel, "File accepted", "File recieved", JOptionPane.INFORMATION_MESSAGE);
+	            	    ScenarioParser s = new ScenarioParser(true);
+	            	    s.setScenarioFile(fileChooser.getSelectedFile().getAbsolutePath());
+	            	  
+
+	            	   
+	               }
+		
+			File file = fileChooser.getSelectedFile();
+			System.out.println("Directory: " + file);
+			FileParser scenarioArray = new FileParser(file);
+			}
     }
 }
