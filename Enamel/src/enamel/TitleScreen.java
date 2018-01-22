0370c80b4a4238d2ad634ff52fd9dd69b51eba21
dansan85 <dansan85@ea33.eecs.yorku.ca>
2018-01-22 13:00:59 -0500

    Added title screen class

--- Enamel/src/enamel/TitleScreen.java
+++ Enamel/src/enamel/TitleScreen.java
@@ -1,7 +1,18 @@
 package enamel;
 
+import javax.swing.*;
+
 public class TitleScreen {
 	
+	
+	public TitleScreen()
+	{
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setSize(640, 480);
+		JPanel panel = new JPanel();
+		
+	}
 	
 	
 
