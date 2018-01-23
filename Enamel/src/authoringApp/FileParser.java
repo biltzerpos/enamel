9bca007e43783157bd69c604c8786fd1a4394f78
qasim98 <qasimmahir@gmail.com>
2018-01-22 22:01:58 -0500

    Made changes in FileParser and used parsedtext array

--- Enamel/src/authoringApp/FileParser.java
+++ Enamel/src/authoringApp/FileParser.java
@@ -1,19 +1,38 @@
 package authoringApp;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FileParser {
 	
 	private String[] parsedText;
 	
 	// Constructor
-	public FileParser(File file) {
+	public FileParser(File file) throws IOException{
 		// TODO Auto-generated constructor stub
 		parseFileIntoArray(file);
+		
 	}
 
 	// Parse the file and save it into the array parsedText
-	private void parseFileIntoArray(File file) {
+	private void parseFileIntoArray(File file) throws IOException {
+		
+		Scanner scanner = new Scanner(file);
+		
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String string= "";
+        ArrayList<String> lines = new ArrayList<String>();
+        while((string = reader.readLine()) != null){
+            lines.add(string);
+        }
+       parsedText = lines.toArray(new String[lines.size()]);
+       System.out.println(parsedText[2]);
 		
 	}
 	
