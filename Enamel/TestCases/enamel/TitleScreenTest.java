4d0a56b372b2bcf750cf4fcdd3c88fdeb8b3da6f
sohnsteve <stevesohn.ss@gmail.com>
2018-01-24 13:51:46 -0500

    added test class for titlescreen, created separate folder as per prof

---
+++ Enamel/TestCases/enamel/TitleScreenTest.java
@@ -0,0 +1,23 @@
+package enamel;
+
+import static org.junit.Assert.*;
+
+import javax.swing.JFrame;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TitleScreenTest {
+
+	JFrame test = new JFrame();
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testTitleScreen() {
+		//insert test case
+	}
+
+}
