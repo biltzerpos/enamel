e216304b7257f5ca21e13117cb808a29a9d7464e
Yoni Fihrer <yoni@fihrer.com>
2018-01-24 15:48:06 -0500

    Reverted ToyAuthoring back to it's original

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -8,11 +8,9 @@ import javax.swing.filechooser.*;
 
 public class ToyAuthoring {
 	public static void main(String[] args) throws IOException {
-		TitleScreen idk = new TitleScreen();
-	//	ScenarioParser s = new ScenarioParser(true);
-		//s.setScenarioFile("FactoryScenarios/Scenario_1.txt");
+		ScenarioParser s = new ScenarioParser(true);
+		s.setScenarioFile("FactoryScenarios/Scenario_2.txt");
 		
-		JButton skipBtn = new JButton("Skip");
 	}
  
 }
